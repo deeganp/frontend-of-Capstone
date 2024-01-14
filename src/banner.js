a0ab41aa08dcadc45eb1402fb0563eb4b4df992d
deeganp <deeganp.motro@gmail.com>
2024-01-13 21:27:02 -0500

    changed title to reflect correct name

--- src/banner.js
+++ src/banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
   const {user} = useAuth();
   return (
     <div className="banner">
-      <h1>Cinema Exp.</h1>
+      <h1>Movie Exp.</h1>
       <h2> Find your favorite movies and save them for later!</h2>
       <h2>Click on Search Movies to begin!</h2>
       <h3>
