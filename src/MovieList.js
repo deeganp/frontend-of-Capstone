7b09bec8d8d4c845285deb9bacaa662705e153c0
deeganp <deeganp.motro@gmail.com>
2024-01-19 14:34:49 -0500

    added option to see movie favorites

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useAuth } from './AuthContext';
+import { useHistory } from 'react-router-dom';
 import { useToasts } from 'react-toast-notifications';
 import "./MovieList.css"
 
@@ -8,36 +9,52 @@ const MovieList = ({ api, movies }) => {
   const [favorites, setFavorites] = useState([]);
   const { user } = useAuth();
   const { addToast } = useToasts();
+  const history = useHistory();
 
 
   const handleAddFavorite = async (movieName) => {
     try {
-      // Get the username of the currently logged-in user from your authentication context or state
+      // Get the username of the currently logged-in user from your authentication context
       const username = user;
-
       // Call the addFavorite method to add the movie to the user's favorites
       await api.addFavorite(username, movieName);
 
-      // Update the favorites state (if needed) to reflect the changes immediately
-      // You can fetch the updated favorites here and set them in your state
+      // fetch the updated favorites here and set them in state
       const updatedFavorites = await api.getFavorites(username);
       setFavorites(updatedFavorites);
       addToast(`Added ${movieName} to favorites!`, { appearance: 'success', autoDismiss: true });
     } catch (error) {
-      // Handle errors
-      console.error(error);
+      // Handle errors, such as when the user is not logged in
+      console.error('Failed to add favorite', error);
       addToast(`Failed to add ${movieName} to favorites, please try again.`, { appearance: 'error', autoDismiss: true })
     }
+  };
+
+
+  const showDetails = async (ID) => {
+    try {
+      history.push({
+        pathname: '/MovieDetails',
+        state: { movieID: ID },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleClick = async (ID) => {
+    await showDetails(ID);
   };
 
   return (
     <div className='movie-list-container'>
       <ul className='movie-list'>
         {movies.map((movie) => (
-          <li key={movie} className='movie-item'>
+          <li key={movie.imdbID} className='movie-item'>
             <div className='movie-details'>
-              <span className='movie-name'>{movie} </span>
-              <button className='button-29' onClick={() => handleAddFavorite(movie)}>Favorite</button>
+              <span className='movie-name'>{movie.title} </span>
+              <button className='button-29' onClick={() => handleClick(movie.imdbID)}>Details</button>
+              <button className='button-29' onClick={() => handleAddFavorite(movie.title)}>Favorite</button>
             </div>
           </li>
         ))}
