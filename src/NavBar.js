53109d367ba3f1e72cc0addc74dbe47953075214
deeganp <deeganp.motro@gmail.com>
2024-02-14 16:54:09 -0500

    added logout to app

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { Navbar, Nav, NavItem } from "reactstrap";
+import { useAuth } from './AuthContext';
+import { useToasts } from 'react-toast-notifications';
 import './NavBar.css'
 
 
 function NavBar() {
+    const { logout } = useAuth();
+    const { user } = useAuth();
+    const { addToast } = useToasts();
+
+    const handleLogout = () => {
+        // Call the logout function from AuthContext
+        if (user) {
+            try {
+                logout();
+
+                //  remove JWT from local storage
+                localStorage.removeItem('token');
+                addToast('Logout successful!', { appearance: 'success', autoDismiss: true });
+            } catch (error) {
+                addToast('Logout failed. Please try again.', { appearance: 'error', autoDismiss: true });
+                console.error('Logout failed', error);
+            }
+        } else {
+            addToast('You are not currently signed in.', { appearance: 'info', autoDismiss: true });
+        }
+    };
     return (
         <div>
             <Navbar expand="md" className="navbar-style">
@@ -21,6 +44,9 @@ function NavBar() {
                     </NavItem>
                     <NavItem className="nav-item">
                         <NavLink to="/favorites" className="nav-link" activeClassName="active">Favorite Movies!</NavLink>
+                    </NavItem>
+                    <NavItem className="nav-item">
+                        <button onClick={handleLogout} className="nav-link" activeClassName="active">Logout</button>
                     </NavItem>
                 </Nav>
             </Navbar>
