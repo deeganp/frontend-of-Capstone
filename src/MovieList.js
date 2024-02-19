170be3e5e5ce006cb96eed3e21859f7b2e55678e
deeganp <deeganp.motro@gmail.com>
2024-02-19 12:55:39 -0500

    rewrote movie class to vastly improve movie details page

--- src/MovieList.js
+++ src/MovieList.js
@@ -36,19 +36,19 @@ const MovieList = ({ api, movies }) => {
   };
 
 
-  const showDetails = async (ID) => {
+  const showDetails = async (movie) => {
     try {
       history.push({
         pathname: '/MovieDetails',
-        state: { movieID: ID },
+        state: {movie} ,
       });
     } catch (error) {
       console.error(error);
     }
   };
 
-  const handleClick = async (ID) => {
-    await showDetails(ID);
+  const handleClick = async (movie) => {
+    await showDetails(movie);
   };
 
   return (
@@ -58,7 +58,7 @@ const MovieList = ({ api, movies }) => {
           <li key={movie.imdbID} className='movie-item'>
             <div className='movie-details'>
               <span className='movie-name'>{movie.title} </span>
-              <button className='button-29' onClick={() => handleClick(movie.imdbID)}>Details</button>
+              <button className='button-29' onClick={() => handleClick(movie)}>Details</button>
               <button className='button-29' onClick={() => handleAddFavorite(movie.title)}>Favorite</button>
             </div>
           </li>
