e36a711728da8fc49a322a35471f0d1796b79557
deeganp <deeganp.motro@gmail.com>
2024-03-16 14:00:35 -0400

    typo

--- src/banner.js
+++ src/banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
         <div className="row gx-5 align-items-center">
           <div className="col-lg-6">
             <div className="mb-5 mb-lg-0 text-center">
-              <h1 className="display-1 lh-1 mb-3">Cinema Exp.</h1>
+              <h1 className="display-1 lh-1 mb-3">Movie Exp.</h1>
               <h3>Find your favorite movies and save them for later!</h3>
               <h3>Click on Search Movies to begin!</h3>
               <h3>{user ? `Welcome, ${user}!` : 'Welcome to the site!'}</h3>
